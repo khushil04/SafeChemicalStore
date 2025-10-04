67a5f878f8e323d35e113dd0f13b783c58d828bf
khushil <119103821+khushil04@users.noreply.github.com>
2025-10-04 13:12:00 +0530

    Update vite.config.ts

--- reactfrontend/vite.config.ts
+++ reactfrontend/vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8000',
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://127.0.0.1:8000',
+  //   },
+  // },
   
 });
